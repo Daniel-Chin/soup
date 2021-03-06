9f26ff5fc282cbd2cd988bf9dc844380fb161bda
Daniel Chin <daniel_chin@yahoo.com>
2021-03-06 17:52:21 +0800

    objective render interval

--- main.m
+++ main.m
@@ -2,11 +2,16 @@ close all;
 
 univPars();
 initSim();
+setRender();
 
+render();
 while 1
   dt = DT * dt_multiplier;
   step();
-  render();
-  % pause(.01);
+  if render_elapsed > SPF
+    render_elapsed = 0;
+    render();
+  end
+  render_elapsed = render_elapsed + dt;
   t = t + dt;
 end
---
+++ setRender.m
@@ -0,0 +1,4 @@
+FPS = 8000;
+
+SPF = 1 / FPS;
+render_elapsed = 0;
