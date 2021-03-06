d0159353eea2eb3d209ed55e10f387b3b2cdb693
Daniel Chin <daniel_chin@yahoo.com>
2021-03-06 23:48:01 +0800

    overnight queue

---
+++ renderQueue.m
@@ -0,0 +1,26 @@
+close all; 
+
+for DIMENSIONS = [2, 3]
+  for N = [3 4 5 6 7 8 9 10 11 12 13 14 17 23 27 33 69]
+    spawn_matrix();
+
+    univPars();
+    initSim();
+    setRender();
+    OUTPUT_DIR = sprintf('D:/soup/output_%dD%d_dyna/', DIMENSIONS, N);
+    mkdir(OUTPUT_DIR);
+    OUTPUT_PATH = OUTPUT_DIR + '%d.png';
+
+    render();
+    while 1
+      step();
+      render();
+      if render_elapsed > SPF || save_oversample
+        render_elapsed = 0;
+        saveRender();
+      end
+      render_elapsed = render_elapsed + dt;
+      t = t + dt;
+    end
+  end
+end
