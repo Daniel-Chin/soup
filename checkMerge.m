e09302037038cfb29b2a00444bf2030486beb0ba
Daniel Chin <daniel_chin@yahoo.com>
2021-03-06 19:27:44 +0800

    optim

--- checkMerge.m
+++ checkMerge.m
@@ -1,36 +1,34 @@
 to_break = 0;
 for k = 1 : N_PARTICLES
-  for kk = 1 : N_PARTICLES
+  relevance = vecnorm((location(k, :) - location)') ./ max(1, vecnorm((location(k, :) + location)') .^ 1.6);
+  for kk = find(relevance < MERGE_THRESHOLD)
     if k == kk
       continue;
     end
-    relevance = norm(location(k, :) - location(kk, :)) / max(1, norm(location(k, :) + location(kk, :)) ^ 2);
-    if relevance < MERGE_THRESHOLD
-      % disp("merge");
-      combined_mass = mass(k) + mass(kk);
-      if combined_mass == 0
-        if kk > k
-          id_to_remove = kk;
-          removeParticle();
-          id_to_remove = k;
-          removeParticle();
-        else
-          id_to_remove = k;
-          removeParticle();
-          id_to_remove = kk;
-          removeParticle();
-        end
-        to_break = 1;
-        break;  
+    % disp("merge");
+    disp(N_PARTICLES);
+    combined_mass = mass(k) + mass(kk);
+    if combined_mass == 0
+      if kk > k
+        id_to_remove = kk;
+        removeParticle();
+        id_to_remove = k;
+        removeParticle();
+      else
+        id_to_remove = k;
+        removeParticle();
+        id_to_remove = kk;
+        removeParticle();
       end
+    else
       location(k, :) = (mass(k) * location(k, :) + mass(kk) * location(kk, :)) / combined_mass;
       velocity(k, :) = (mass(k) * velocity(k, :) + mass(kk) * velocity(kk, :)) / combined_mass;
       mass(k) = combined_mass;
       id_to_remove = kk;
       removeParticle();
-      to_break = 1;
-      break;  
     end
+    to_break = 1;
+    break;  
   end
   if to_break
     break;
