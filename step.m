90989f5b8d44558982256c4b5fe0d22df1043fc6
Daniel Chin <daniel_chin@yahoo.com>
2021-03-06 18:23:07 +0800

    dynamic timestepping via max velo

--- step.m
+++ step.m
@@ -1,11 +1,11 @@
-min_distance_3 = inf;
+% min_distance_3 = inf;
 
 force_G = zeros(N_PARTICLES, 3);
 for k = 1 : N_PARTICLES
   displace = location - location(k, :);
   distance_3 = sum(displace .^ 2, 2) .^ 1.5;
   distance_3(k) = inf;  % any non-zero
-  min_distance_3 = min(min_distance_3, min(distance_3));
+  % min_distance_3 = min(min_distance_3, min(distance_3));
   force_G(k, :) = sum(mass_product(:, k) .* displace ./ distance_3, 1);
 end
 force = force_G * G;
@@ -13,7 +13,8 @@ force = force_G * G;
 velocity = velocity + (force ./ mass) * dt;
 location = location + velocity * dt;
 
-dt_multiplier = min_distance_3 ^ .5;
+% dt_multiplier = min_distance_3 ^ .5;
+dt_multiplier = 1 / max(vecnorm(force'));
 
 if mod(step_i , 16) == 0
   checkMerge();
