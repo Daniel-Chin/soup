15801104c46ccb854ce50b01e6c22efff2925140
Daniel Chin <daniel_chin@yahoo.com>
2021-03-07 10:22:52 +0800

    ffmpeg queue

---
+++ ffQueue.bat
@@ -0,0 +1,28 @@
+call ./folder2vid.bat output_2D11_dyna
+call ./folder2vid.bat output_2D12_dyna
+call ./folder2vid.bat output_2D13_dyna
+call ./folder2vid.bat output_2D14_dyna
+call ./folder2vid.bat output_2D17_dyna
+call ./folder2vid.bat output_2D23_dyna
+call ./folder2vid.bat output_2D27_dyna
+call ./folder2vid.bat output_2D33_dyna
+call ./folder2vid.bat output_2D69_dyna
+
+call ./folder2vid.bat output_3D3_dyna
+call ./folder2vid.bat output_3D4_dyna
+call ./folder2vid.bat output_3D5_dyna
+call ./folder2vid.bat output_3D6_dyna
+call ./folder2vid.bat output_3D7_dyna
+call ./folder2vid.bat output_3D8_dyna
+call ./folder2vid.bat output_3D9_dyna
+call ./folder2vid.bat output_3D10_dyna
+call ./folder2vid.bat output_3D11_dyna
+call ./folder2vid.bat output_3D12_dyna
+call ./folder2vid.bat output_3D13_dyna
+call ./folder2vid.bat output_3D14_dyna
+call ./folder2vid.bat output_3D17_dyna
+call ./folder2vid.bat output_3D23_dyna
+call ./folder2vid.bat output_3D27_dyna
+call ./folder2vid.bat output_3D33_dyna
+call ./folder2vid.bat output_3D69_dyna
+pause
